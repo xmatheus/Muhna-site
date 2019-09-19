bed87d3d9dba5022c5cda313ac0adc64be4f47c6
Matheus <matheuscorreia559@gmail.com>
2019-09-18 20:23:26 -0400

    adicionando icones e animação na barra da esquerda

--- src/pages/dashboard/BarLeft/index.js
+++ src/pages/dashboard/BarLeft/index.js
@@ -1,46 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import { FaNewspaper, FaQrcode, FaUsers } from "react-icons/fa";
 
 // import { Container } from './styles';
-import './styles.css';
+import "./styles.css";
 
-import { getData } from '../../services/auth';
+import { getData } from "../../services/auth";
 
-import logo from '../../../assets/logo.png';
+import logo from "../../../assets/logo.png";
 
 export default class BarLeft extends Component {
 	state = {
-	    data: []
+		data: []
 	};
 
 	componentDidMount = () => {
-	    const data = getData();
-	    this.setState({ data: data });
+		const data = getData();
+		this.setState({ data: data });
 	};
 
 	render() {
-	    return (
-	        <div className="barleft-main-div">
-	            {this.state.data ? (
-	                <div>
-	                    <img src={logo}></img>
-	                    <p id="p-hello-visit">
-	                        {'Olá, ' + this.state.data.name + '.'}
-	                    </p>
-	                    <div className="menu-items">
-	                        <p>Notícias</p>
-	                        <hr />
-	                        <p>Visita guiada</p>
-	                        <hr />
-	                        {this.state.data.isAdmin ? (
+		return (
+			<div className="barleft-main-div">
+				{this.state.data ? (
+					<div>
+						<img id="img-div" src={logo}></img>
+						<p id="p-hello-visit">
+							{"Olá, " + this.state.data.name + "."}
+						</p>
+						<div className="menu-items">
+							<div className="div-horizontal">
+								<FaNewspaper color="#ffffff" size={25} />
+								<p>Notícias</p>
+							</div>
+
+							<div className="div-horizontal">
+								<FaQrcode color="#ffffff" size={25} />
+								<p>Visita guiada</p>
+							</div>
+							{this.state.data.isAdmin ? (
 								<>
-									<p>Usuários</p>
-									<hr />
+									<div className="div-horizontal">
+										<FaUsers color="#ffffff" size={25} />
+										<p>Usuários</p>
+									</div>
 								</>
-	                        ) : null}
-	                    </div>
-	                </div>
-	            ) : null}
-	        </div>
-	    );
+							) : null}
+						</div>
+					</div>
+				) : null}
+			</div>
+		);
 	}
 }
