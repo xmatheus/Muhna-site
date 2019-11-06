5051faab15a2c69ea345717a034aa93da5ab4674
Matheus <matheuscorreia559@gmail.com>
2019-11-06 00:02:03 -0300

    Updates

---
+++ precache-manifest.adc1e3aa4a1c20da87ac0e01fc369d72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327c80416a07357e0597f43ed1ad718b",
+    "url": "/internal-Muhna/index.html"
+  },
+  {
+    "revision": "ac127c45b07ec21a95e9",
+    "url": "/internal-Muhna/static/css/2.d087049b.chunk.css"
+  },
+  {
+    "revision": "a4f15a5bf1e1660fe0ae",
+    "url": "/internal-Muhna/static/css/main.94b0861f.chunk.css"
+  },
+  {
+    "revision": "ac127c45b07ec21a95e9",
+    "url": "/internal-Muhna/static/js/2.3bad3bfb.chunk.js"
+  },
+  {
+    "revision": "a4f15a5bf1e1660fe0ae",
+    "url": "/internal-Muhna/static/js/main.7602671a.chunk.js"
+  },
+  {
+    "revision": "a82cdc037002a884df3d",
+    "url": "/internal-Muhna/static/js/runtime~main.1921cca8.js"
+  },
+  {
+    "revision": "140acc207a47f389024d4eb302043f0c",
+    "url": "/internal-Muhna/static/media/yt.140acc20.jpg"
+  }
+]);
